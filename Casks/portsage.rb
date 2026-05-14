ff4917f804b331da1ea0787a0f0535a2f26046da
Simone Salerno <doppiaesse@proton.me>
2026-05-14 16:12:01 +0200

    Update portsage to v0.12.1

--- Casks/portsage.rb
+++ Casks/portsage.rb
@@ -1,6 +1,6 @@
 cask "portsage" do
-  version "0.12.0"
-  sha256 "517f9660eca5f46a0c03f2a1180074dc66921e68b9d29b446409da03f10f852a"
+  version "0.12.1"
+  sha256 "775a26a0e0e2e1fd25725ff4fc7130f9a9d6c957ccf8647b3ca486478b686d04"
 
   url "https://github.com/essedev/portsage/releases/download/v#{version}/Portsage_#{version}_aarch64.dmg"
   name "Portsage"
